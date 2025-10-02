6f0554f50b8fabdc14e13eeb6c043528b82bafe1
tj003 <tj0032@naver.com>
2025-10-02 15:35:07 +0900

    1002

---
+++ shop/src/test/java/edu/sm/sakljfsafApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class sakljfsafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
